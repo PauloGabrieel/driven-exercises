afed40077b85084d615212f915c619408715ae59
Paulo Gabriel <89712465+PauloGabrieel@users.noreply.github.com>
2022-06-14 20:51:26 -0300

    feat: implement solution for primeiro-servidor exercise

---
+++ primeiro-servidor/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+const server = express();
+
+server.get("/hello",(request, response)=>{
+    response.send("**Meu primeiro servidor, yay!**");
+})
+
+
+
+server.listen(5000);
